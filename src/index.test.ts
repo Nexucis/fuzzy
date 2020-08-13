1a54fe47bc38a927233e597669b9a345b4bf085c
Augustin Husson <husson.augustin@gmail.com>
2020-08-13 14:27:08 +0200

    implement render method + starting to implement match method
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,9 +2,65 @@ import { Fuzzy, FuzzyResult } from './index';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('dummy test', ()=> {
-    it('should pass properly' , () => {
+describe('render test', () => {
+    it('default conf: shouldn t alter the string', () => {
         const fuz = new Fuzzy({})
-        expect(fuz.match('','')).to.deep.equal({} as FuzzyResult);
+        expect(fuz.render('my awesome text', [{from: 0, to: 14}]))
+            .to.equal('my awesome text')
     });
+    it('conf used to enable the rendering, full match', () => {
+        const fuz = new Fuzzy({escapeHTML: true, pre: '<b style="color: brown">', post: '</b>'})
+        expect(fuz.render('<p>target is 100% of coverage</p>', [{from: 0, to: 32}]))
+            .to.equal('<b style="color: brown">&lt;p&gt;target is 100% of coverage&lt;/p&gt;</b>')
+    });
+    it('conf used to enable the rendering, partial match', () => {
+        const fuz = new Fuzzy({escapeHTML: true, pre: '<b style="color: brown">', post: '</b>'})
+        expect(fuz.render('target is <p>100% of coverage</p>', [{from: 10, to: 11}, {from: 15, to: 18}]))
+            .to.equal('target is <b style="color: brown">&lt;p</b>&gt;10<b style="color: brown">0% o</b>f coverage&lt;/p&gt;')
+    })
+})
+
+describe('perfect match test', () => {
+    it('default conf', () => {
+        const fuz = new Fuzzy({})
+        expect(fuz.match('my awesome text', 'my awesome text'))
+            .to.deep.equal({
+            original: 'my awesome text',
+            rendered: 'my awesome text',
+            score: Infinity
+        } as FuzzyResult);
+    });
+    it('default conf with html text', () => {
+        const fuz = new Fuzzy({})
+        expect(fuz.match('<p>target is 100% of coverage</p>', '<p>target is 100% of coverage</p>'))
+            .to.deep.equal({
+            original: '<p>target is 100% of coverage</p>',
+            rendered: '<p>target is 100% of coverage</p>',
+            score: Infinity
+        } as FuzzyResult)
+    });
+    it('conf used to enable the rendering', () => {
+        const fuz = new Fuzzy({escapeHTML: true, includeMatches: true, pre: '<b style="color: brown">', post: '</b>'})
+        expect(fuz.match('<p>target is 100% of coverage</p>', '<p>target is 100% of coverage</p>'))
+            .to.deep.equal({
+            original: '<p>target is 100% of coverage</p>',
+            rendered: '<b style="color: brown">&lt;p&gt;target is 100% of coverage&lt;/p&gt;</b>',
+            score: Infinity,
+            indices: [{from: 0, to: 32}]
+        } as FuzzyResult)
+    });
+    it('case insensitive', () => {
+        const fuz = new Fuzzy({})
+        expect(fuz.match('This Is So Cool', 'thIs iS sO cOOl'))
+            .to.deep.equal({
+            original: 'thIs iS sO cOOl',
+            rendered: 'thIs iS sO cOOl',
+            score: Infinity
+        } as FuzzyResult)
+    })
+    it('case sensitive', () => {
+        const fuz = new Fuzzy({caseSensitive: true})
+        expect(fuz.match('This Is So Cool', 'thIs iS sO cOOl'))
+            .to.equal(null)
+    })
 })
--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,23 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+function escapeHTML(text: string): string {
+    return text.replace(/[&<>"']/g, (m: string) => {
+        switch (m) {
+            case '&':
+                return '&amp;';
+            case '<':
+                return '&lt;';
+            case '>':
+                return '&gt;';
+            case '"':
+                return '&quot;';
+            default:
+                return '&#039;';
+        }
+    });
+}
+
 export interface FuzzyConfiguration {
     caseSensitive?: boolean;
     includeMatches?: boolean;
@@ -31,21 +48,85 @@ export interface FuzzyConfiguration {
 }
 
 export interface FuzzyResult {
-    rendered?: string;
+    rendered: string;
     original: string;
     score: number;
     indices?: { from: number; to: number }[];
 }
 
 export class Fuzzy {
-    private readonly conf: FuzzyConfiguration
+    private readonly caseSensitive: boolean;
+    private readonly includeMatches: boolean;
+    private readonly shouldSort: boolean;
+    private readonly escapeHTML: boolean;
+    private readonly pre: string
+    private readonly post: string;
 
     constructor(conf: FuzzyConfiguration) {
-        this.conf = conf
+        this.caseSensitive = conf.caseSensitive === undefined ? false : conf.caseSensitive;
+        this.includeMatches = conf.includeMatches === undefined ? false : conf.includeMatches;
+        this.shouldSort = conf.shouldSort === undefined ? false : conf.shouldSort;
+        this.escapeHTML = conf.escapeHTML === undefined ? false : conf.escapeHTML;
+        this.pre = conf.pre === undefined ? '' : conf.pre;
+        this.post = conf.post === undefined ? '' : conf.post;
     }
 
     // match will return a result if `pattern` is matching `text`,
-    match(pattern: string, text: string): FuzzyResult {
-        return {} as FuzzyResult;
+    match(pattern: string, text: string): FuzzyResult | null {
+        let localPattern = pattern
+        let localText = text
+        if (!this.caseSensitive) {
+            localPattern = localText.toLowerCase()
+            localText = localText.toLowerCase()
+        }
+        // in case it's a perfect match, no need to loop to find which char is matching
+        if (localPattern === localText) {
+            const indices = [{from: 0, to: pattern.length - 1}]
+            const result = {
+                original: text,
+                rendered: this.render(text, indices),
+                score: Infinity
+            } as FuzzyResult
+            if (this.includeMatches) {
+                result.indices = indices
+            }
+            return result
+        }
+        return null;
+    }
+
+    render(text: string, indices: { from: number, to: number }[]): string {
+        let rendered = ''
+        for (let i = 0; i < indices.length; i++) {
+            const currentInterval = indices[i]
+            let previousNotMatchingInterval = null;
+            if (i === 0 && currentInterval.from !== 0) {
+                previousNotMatchingInterval = {from: 0, to: currentInterval.from - 1}
+            }
+            if (i > 0) {
+                previousNotMatchingInterval = {from: indices[i - 1].to + 1, to: currentInterval.from - 1}
+            }
+            let previousStr = ''
+            if (previousNotMatchingInterval !== null) {
+                previousStr = this.extractSubString(text, previousNotMatchingInterval)
+            }
+            const currentStr = this.extractSubString(text, currentInterval)
+            rendered = rendered + previousStr + this.pre + currentStr + this.post
+        }
+
+        // check if the last interval contains the end of the string. Otherwise add it
+        const lastInterval = indices[indices.length - 1]
+        if (lastInterval.to < text.length - 1) {
+            rendered = rendered + this.extractSubString(text, {from: lastInterval.to + 1, to: text.length})
+        }
+        return rendered
+    }
+
+    private extractSubString(text: string, interval: { from: number, to: number }) {
+        let str = text.substr(interval.from, interval.to - interval.from + 1)
+        if (this.escapeHTML) {
+            str = escapeHTML(str)
+        }
+        return str
     }
 }
